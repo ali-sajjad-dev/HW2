2691abb9bbfe0ff8982187ee1c25d188b6de3c6b
asajjad38 <49500921+asajjad38@users.noreply.github.com>
2020-11-09 11:14:05 -0500

    Update stack.h

--- problem 4/stack.h
+++ problem 4/stack.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "Node.h"
 
+// generic stack of type T
 template <typename T>
 class Stack
 {
 public:
+	// data members
+	// pointers to nodes and size
 	Node<T>* top;
 	Node<T>* bottom;
 	int size;
@@ -16,12 +19,16 @@ public:
 		bottom = nullptr;
 		size = 0;
 	}
-
+	
+	// add data to top of stack
 	void push(T data);
 
+	// remove top most node
 	void pop();
-
+	
+	// insert data to specified index
 	void insert(int index, T type);
-
+	
+	// print out contents of stack to console
 	void printStack();
 };
