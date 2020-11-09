f565abf6fe2d2f1f0e8d4aa30cb9368843e7a6fc
asajjad38 <49500921+asajjad38@users.noreply.github.com>
2020-11-09 11:12:43 -0500

    Update stack.cpp

--- problem 4/stack.cpp
+++ problem 4/stack.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 int main()
 {
+	// create stack object of integer type
 	Stack<int> myStack;
-
+	
+	// operations to test our stack making sure everything works.
 	myStack.push(12);
 	myStack.push(13);
 	myStack.push(27);
 	myStack.push(19);
 	myStack.push(313);
 	myStack.printStack();
-	//myStack.pop();
-	//myStack.pop();
+	myStack.pop();
+	myStack.pop();
 	cout << endl;
 	myStack.insert(3, 79);
 	myStack.printStack();
@@ -29,6 +31,7 @@ void Stack<T>::push(T data)
 	// add first
 	if (size == 0)
 	{
+		// new node as top
 		top = new Node<T>(data);
 		bottom = top;
 		size++;
@@ -47,6 +50,7 @@ void Stack<T>::push(T data)
 template<typename T> 
 void Stack<T>::pop()
 {
+	// make sure to see if stack is not empty
 	if (size == 0)
 	{
 		cout << "Cannot pop off an empty stack" << endl;
@@ -54,6 +58,8 @@ void Stack<T>::pop()
 	}
 	else
 	{
+		// create tmp node and assign it to node after top
+		// remove top and reassign top.
 		Node<T>* tmp;
 		tmp = top->next;
 		delete top;
@@ -62,12 +68,16 @@ void Stack<T>::pop()
 	}
 }
 
+// print out the contents of stack from Top -> Bottom
 template<typename T>
 void Stack<T>::printStack()
 {
+	// tmp node
 	Node<T>* tmp = top;
 
 	cout << "Top " << endl;
+	
+	// traverse so as long as temp pointer isnt pointing to nothing.
 	while (tmp != nullptr)
 	{
 		cout << "__" << endl;
@@ -78,7 +88,8 @@ void Stack<T>::printStack()
 	cout << "Bottom" << endl;
 }
 
-
+// insert a data into the stack at specified index.
+// Index 0 being the top and bottom being size-1
 template<typename T>
 void Stack<T>::insert(int index, T data)
 {
@@ -88,12 +99,17 @@ void Stack<T>::insert(int index, T data)
 		cout << "Invalid index" << endl;
 		return;
 	}
+	// if the index is top then call push()
 	else if (index == 0)
 	{
 		push(data);
 	}
 	else
 	{
+		// otherwise traverse to node right before index of interest
+		// make new node point to node at index of interst
+		// make previous node reference new node
+		// this is done in this specific order so as to not orphan the stack and to avoid memory leaks
 		Node<T>* tmp = top;
 		for (int i = 0; i < index-1; i++)
 		{
