40cc973638f6a1c0aff480977c87a1cf7dad9050
asajjad38 <49500921+asajjad38@users.noreply.github.com>
2020-11-09 11:07:49 -0500

    Update Node.h

--- problem 4/Node.h
+++ problem 4/Node.h
@@ -5,15 +5,19 @@ template <typename T>
 class Node
 {
 public:
+	// data in node of type T
 	T data;
+	// pointer to the next node
 	Node<T>* next;
-
+	
+	// default constructor
 	Node()
 	{
 		this->data = -1;
 		this->next = nullptr;
 	}
-
+	
+	// constructor w/ data input
 	Node(T data)
 	{
 		this->data = data;
