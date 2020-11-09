1f80b7ba8974b07282a48c3e262ba2e92fa4792a
asajjad38 <49500921+asajjad38@users.noreply.github.com>
2020-11-09 11:05:28 -0500

    Update Stack.h

--- problem 7/Stack.h
+++ problem 7/Stack.h
@@ -3,10 +3,13 @@
 
 using namespace std;
 
+// generic stack class
 template <class T>
 class Stack
 {
 public:
+	// data members
+	// pointers and size of stack
 	MoveNode<T>* top;
 	MoveNode<T>* bottom;
 	int size;
@@ -18,17 +21,23 @@ public:
 		bottom = nullptr;
 		size = 0;
 	}
-
+	
+	// add a new element to top
 	void push(T x, T y);
 
+	// remove top most element
 	void pop();
 
+	// insert element at given index
 	void insert(int index, T x, T y);
 
+	// print out the entire maze path
 	void print_Stack_path();
 
+	// check to see if a move is legal
 	bool isLegal(T maze[][5], int row, int col);
-
+	
+	// solve the given maze
 	void solveMaze(T maze[][5], int row, int col);
 
 };
