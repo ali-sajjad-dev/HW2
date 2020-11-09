11a3c2787abb1565377b86bec7f3ffbf5e8be452
asajjad38 <49500921+asajjad38@users.noreply.github.com>
2020-11-09 11:06:59 -0500

    Update moveNode.h

--- problem 7/moveNode.h
+++ problem 7/moveNode.h
@@ -2,13 +2,18 @@
 
 using namespace std;
 
+// generic node for each move
 template<typename T>
 class MoveNode
 {
 public:
+	// each move corresponds to a grid coordinate
+	// hence a 1D array of 2 elements
 	T move[2];
+	// pointer to next node
 	MoveNode* next;
-
+	
+	// node constructor
 	template<typename T>
 	MoveNode(T x, T y)
 	{
@@ -16,7 +21,8 @@ public:
 		move[1] = y;
 		next = nullptr;
 	}
-
+	
+	// compare two nodes, i.e. 2 coordinates 
 	bool compare(MoveNode a);
 };
 
